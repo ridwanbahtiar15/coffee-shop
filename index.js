df5cfc29d3903c04ab7977eb8871c57ff583a673
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2023-09-17 04:28:31 +0700

    add cors

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const mainRouter = require("./src/Routers/main.router");
 app.use(mainRouter);
 

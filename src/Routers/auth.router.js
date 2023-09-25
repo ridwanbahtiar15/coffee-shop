3e5ad3ce2c1e1cb3da1877fd88add9a2099c97d5
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2023-09-26 01:00:25 +0700

    fix

--- src/Routers/auth.router.js
+++ src/Routers/auth.router.js
@@ -1,9 +1,8 @@
 const authRouter = require("express").Router();
 const { register, login, activation } = require("../Handlers/auth.handler");
-const { singleUpload } = require("../Middlewares/diskUpload");
 
 authRouter.get("/activation", activation);
-authRouter.post("/register", singleUpload("users_image"), register);
+authRouter.post("/register", register);
 authRouter.post("/login", login);
 
 module.exports = authRouter;

9e04f20cb6e63eeed777f14c5d3b9df5fe2833b7
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2024-01-18 02:37:33 +0700

    fix cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 // cors
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["POST", "PATCH", "DELETE"],
   })
 );
